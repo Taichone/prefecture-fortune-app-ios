5f5bdeae7c8059a84e1bf6e951b46d5556142872
Taichone <taichone.dev@gmail.com>
2024-06-06 20:24:36 +0900

    Xcode Project 作成

---
+++ PrefectureFortuneApp/PrefectureFortuneApp/PrefectureFortuneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefectureFortuneAppApp.swift
+//  PrefectureFortuneApp
+//
+//  Created by Taichi on 2024/06/06.
+//
+
+import SwiftUI
+
+@main
+struct PrefectureFortuneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
