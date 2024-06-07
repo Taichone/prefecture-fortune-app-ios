e8cb78c89e3bed5ee7c0e4f709e173eebbf8262b
Taichone <taichone.dev@gmail.com>
2024-06-07 14:56:49 +0900

    add: API request / response 用の構造体を定義

--- PrefectureFortuneApp/PrefectureFortuneApp/Service/FortuneAPIClient.swift
+++ PrefectureFortuneApp/PrefectureFortuneApp/Service/FortuneAPIClient.swift
@@ -8,4 +8,20 @@
 import Foundation
 import Alamofire
 
-struct FortuneAPIClient {}
+struct FortuneAPIClient {
+    private struct RequestBody: Encodable {
+        let name: String
+        let birthday: YearMonthDay
+        let blood_type: String
+        let today: YearMonthDay
+    }
+
+    private struct ResponseBody: Decodable {
+        let name: String
+        let has_coast_line: Bool
+        let citizen_day: MonthDay?
+        let capital: String
+        let logo_url: String
+        let brief: String
+    }
+}
