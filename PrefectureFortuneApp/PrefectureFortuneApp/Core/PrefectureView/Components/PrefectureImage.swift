2a84bc9f8ebe0d82e47b114fe0e8bfbcf2c7b947
Taichone <taichone.dev@gmail.com>
2024-06-22 18:52:29 +0900

    fix: 都道府県画像の表示には、phase が cancel 時にもリロードするよう細工した AsyncImage を利用

--- PrefectureFortuneApp/PrefectureFortuneApp/Core/PrefectureView/Components/PrefectureImage.swift
+++ PrefectureFortuneApp/PrefectureFortuneApp/Core/PrefectureView/Components/PrefectureImage.swift
@@ -9,45 +9,25 @@ import SwiftUI
 
 struct PrefectureImage: View {
     let imageUrl: String
-    @State private var imagePhase: AsyncImagePhase = .empty
 
     var body: some View {
-        VStack {
-            switch self.imagePhase {
-            case .empty:
-                ProgressView()
-                    .onAppear { self.loadImage() }
-            case .success(let image):
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            case .failure:
-                EmptyImage()
-                    .onAppear { self.loadImage() }
-            @unknown default:
-                EmptyImage()
-                    .onAppear { self.loadImage() }
-            }
-        }
-    }
-
-    private func loadImage() {
-        Task {
-            guard let url = URL(string: self.imageUrl) else {
-                self.imagePhase = .failure(URLError(.badURL))
-                return
-            }
-
-            do {
-                let (data, _) = try await URLSession.shared.data(from: url)
-                if let uiImage = UIImage(data: data) {
-                    self.imagePhase = .success(Image(uiImage: uiImage))
-                } else {
-                    self.imagePhase = .failure(URLError(.cannotDecodeContentData))
+        if let url = URL(string: self.imageUrl) {
+            AsyncImage(url: url) { phase in
+                switch phase {
+                case .empty:                
+                    ProgressView()
+                case .success(let image):
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                case .failure:
+                    PrefectureImage(imageUrl: self.imageUrl)
+                @unknown default:
+                    EmptyView()
                 }
-            } catch {
-                self.imagePhase = .failure(error)
             }
+        } else {
+            Text("Invalid Image URL 😭")
         }
     }
 }
